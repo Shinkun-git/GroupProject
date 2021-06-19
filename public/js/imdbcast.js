d8138bb702145058ede7841f62a8842c109b2d01
Prashant kr. Sinku <prshntksinku7@gmail.com>
2021-06-19 19:45:27 +0530

    chngd about-PG, rmvd bk2top, delte imdbcstJS

--- public/js/imdbcast.js
+++
@@ -1,8 +0,0 @@
-
-
-
-const cform = document.querySelector("cform")
-cform.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    console.log(cform.elements.cst.value)
-})
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 
 const User = require('../models/User')
 const  isLog = require('../middleware/isLog')
+const  WrapAsync = require('../middleware/WrapAsync')
 
 router.get('/register', (req, res) => {
     res.render('register')
@@ -12,13 +13,13 @@ router.get('/login', (req, res) => {
     res.render('login')
 })
 
-router.post('/register', async (req, res, next) => {
+router.post('/register',WrapAsync(async (req, res, next) => {
     const { username, email, password } = req.body;
     const newUser = await new User({ username, email })
     const RegUser = await User.register(newUser, password)
     req.login(RegUser, (err) => { if (err) { next(err) } });
     res.redirect('/');
-})
+}))
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Logged IN successfully!!')
